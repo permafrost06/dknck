50182a5af80534a12c642c54808a47b5b205c2b8
Saad <sakib.saad.khangmail.com>
2023-12-24 00:45:38 +0600

    remove header & sales table

--- resources/js/pages/admin/dashboard.js
+++ resources/js/pages/admin/dashboard.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue';
 import ItemsTable from '@/views/admin/index/ItemsTable.vue';
-import SalesTable from '@/views/admin/sales/SalesTable.vue';
 
 const itemsApp = createApp(ItemsTable);
 itemsApp.mount('#items-table');
-
-const salesApp = createApp(SalesTable);
-salesApp.mount('#sales-table');
--- resources/views/admin/index/dashboard.blade.php
+++ resources/views/admin/index/dashboard.blade.php
@@ -21,31 +21,6 @@
         ]" :items="[]" active="" />
     </div>
     <x-cards.card>
-        <div class="flex items-center px-6 py-3 border-b">
-            <h3 class="flex-grow text-lg text-gray-600 font-semibold">Stock Items</h3>
-            <a class="inline-flex items-center px-3 py-1.5 bg-skin-accent focus:ring ring-skin-accent hover:bg-skin-accent-hover text-skin-inverted uppercase font-semibold text-xs rounded"
-                href="{{ route('items.add') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 12 12">
-                    <path fill="currentColor"
-                        d="M6.5 1.75a.75.75 0 0 0-1.5 0V5H1.75a.75.75 0 0 0 0 1.5H5v3.25a.75.75 0 0 0 1.5 0V6.5h3.25a.75.75 0 0 0 0-1.5H6.5V1.75Z" />
-                </svg> Add
-            </a>
-        </div>
-
         <div class="px-6 py-3" id="items-table"></div>
-    </x-cards.card>
-    <x-cards.card class="mt-4">
-        <div class="flex items-center px-6 py-3 border-b">
-            <h3 class="flex-grow text-lg text-gray-600 font-semibold">Sales</h3>
-            <a class="inline-flex items-center px-3 py-1.5 bg-skin-accent focus:ring ring-skin-accent hover:bg-skin-accent-hover text-skin-inverted uppercase font-semibold text-xs rounded"
-                href="{{ route('sales.add') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 12 12">
-                    <path fill="currentColor"
-                        d="M6.5 1.75a.75.75 0 0 0-1.5 0V5H1.75a.75.75 0 0 0 0 1.5H5v3.25a.75.75 0 0 0 1.5 0V6.5h3.25a.75.75 0 0 0 0-1.5H6.5V1.75Z" />
-                </svg> Add
-            </a>
-        </div>
-
-        <div class="px-6 py-3" id="sales-table"></div>
     </x-cards.card>
 @endsection
