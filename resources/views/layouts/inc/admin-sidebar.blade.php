b6b07f3d5042573384a5406a3e844a7545396cf7
Saad <sakib.saad.khangmail.com>
2023-12-24 00:44:10 +0600

    fix: reorganize sidebar

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -35,6 +35,14 @@
                 </x-slot:svg>
                 Dashboard
             </x-sidebar-item>
+            
+            <x-sidebar-item route="items.add">
+                <x-slot:svg viewBox="0 0 20 20">
+                    <path fill="currentColor"
+                        d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16z" />
+                </x-slot:svg>
+                Add Item
+            </x-sidebar-item>
 
             <x-sidebar-item route="items.by_vendor" link="#">
                 <x-slot:svg viewBox="0 0 16 16">
@@ -44,7 +52,14 @@
                 Stock
             </x-sidebar-item>
 
-            <x-sidebar-item-group :routes="['sales.add', 'sales.index']">
+            <x-sidebar-item route="sales.add">
+                <x-slot:svg viewBox="0 0 20 20">
+                    <path fill="currentColor"
+                        d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16z" />
+                </x-slot:svg>
+                Add Sale
+            </x-sidebar-item>
+            <x-sidebar-item route="sales.index">
                 <x-slot:svg viewBox="0 0 48 48">
                     <g fill="none">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"
@@ -55,40 +70,17 @@
                         <circle cx="31" cy="26" r="2" fill="currentColor" />
                     </g>
                 </x-slot:svg>
-                <x-slot:label>
-                    Sales
-                </x-slot:label>
-
-                <x-sidebar-item route="sales.add">
-                    <x-slot:svg viewBox="0 0 20 20">
-                        <path fill="currentColor"
-                            d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16z" />
-                    </x-slot:svg>
-                    Add Sale
-                </x-sidebar-item>
-                <x-sidebar-item route="sales.index">
-                    <x-slot:svg viewBox="0 0 48 48">
-                        <g fill="none">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"
-                                d="M5 17h38l-4.2 26H9.2zm30 0c0-6.627-4.925-12-11-12s-11 5.373-11 12" />
-                            <circle cx="17" cy="26" r="2" fill="currentColor" />
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"
-                                d="M18 33s2 3 6 3s6-3 6-3" />
-                            <circle cx="31" cy="26" r="2" fill="currentColor" />
-                        </g>
-                    </x-slot:svg>
-                    Sales List
-                </x-sidebar-item>
-
-            </x-sidebar-item-group>
-
-            <x-sidebar-item route="items.add">
-                <x-slot:svg viewBox="0 0 20 20">
-                    <path fill="currentColor"
-                        d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16z" />
-                </x-slot:svg>
-                Add Item
+                Sales List
             </x-sidebar-item>
+
+            
+            <x-sidebar-item route="settings.index">
+                <x-slot:svg viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97c0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1c0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64z"/>
+                </x-slot:svg>
+                Settings
+            </x-sidebar-item>
+
 
         </ul>
     </nav>
