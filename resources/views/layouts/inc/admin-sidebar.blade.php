0f8864713f07507c12aca4d218351a033c316bae
Saad <sakib.saad.khan@gmail.com>
2023-12-25 15:47:04 +0600

    rename item -> product

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -33,7 +33,7 @@
                 <x-slot:svg viewBox="0 0 20 20">
                     <path fill="currentColor" d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20a10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16a8 8 0 0 0 0 16z" />
                 </x-slot:svg>
-                Add Item
+                Add Product
             </x-sidebar-item>
 
             <x-sidebar-item route="products.stock">
